c47d8059428e4765dd59bae34decabf7bf0b4533
Anmol <anmol.bhonsale6@gmail.com>
2025-10-16 18:18:37 +0530

    delete function

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ function appendToDisplay(input){
 }
 
 function clearDisplay(){
-    display.value =" ";
+    display.value ="";
 }
 
 function calculate(){
     display.value=eval(display.value);
+}
+
+function backspace(){
+    display.value=(display.value.slice(0,-1))||"0";
 }
